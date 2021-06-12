824ad7e173fe8bc6887736cf3dfa8ff2acfca5ca
nikita-nmc <64993754+nikita-nmc@users.noreply.github.com>
2021-06-12 18:02:08 +1200

    Added basic bot command functionality.

--- main.py
+++ main.py
@@ -4,14 +4,11 @@ import time
 import random
 from discord.ext import commands
 
-response = {'pagination': {'limit': 100, 'offset': 0, 'count': 4, 'total': 4}, 'data': [{'flight_date': '2021-06-12', 'flight_status': 'landed', 'departure': {'airport': 'Auckland International', 'timezone': 'Pacific/Auckland', 'iata': 'AKL', 'icao': 'NZAA', 'terminal': 'I', 'gate': '6', 'delay': 20, 'scheduled': '2021-06-12T07:00:00+00:00', 'estimated': '2021-06-12T07:00:00+00:00', 'actual': '2021-06-12T07:19:00+00:00', 'estimated_runway': '2021-06-12T07:19:00+00:00', 'actual_runway': '2021-06-12T07:19:00+00:00'}, 'arrival': {'airport': 'Kingsford Smith', 'timezone': 'Australia/Sydney', 'iata': 'SYD', 'icao': 'YSSY', 'terminal': '1', 'gate': '8', 'baggage': '15', 'delay': None, 'scheduled': '2021-06-12T08:35:00+00:00', 'estimated': '2021-06-12T08:35:00+00:00', 'actual': '2021-06-12T08:29:00+00:00', 'estimated_runway': '2021-06-12T08:29:00+00:00', 'actual_runway': '2021-06-12T08:29:00+00:00'}, 'airline': {'name': 'Air New Zealand', 'iata': 'NZ', 'icao': 'ANZ'}, 'flight': {'number': '101', 'iata': 'NZ101', 'icao': 'ANZ101', 'codeshared': None}, 'aircraft': {'registration': 'ZK-NNB', 'iata': 'A21N', 'icao': 'A21N', 'icao24': 'C8274F'}, 'live': None}, {'flight_date': '2021-06-12', 'flight_status': 'landed', 'departure': {'airport': 'Auckland International', 'timezone': 'Pacific/Auckland', 'iata': 'AKL', 'icao': 'NZAA', 'terminal': 'I', 'gate': '5', 'delay': 22, 'scheduled': '2021-06-12T09:00:00+00:00', 'estimated': '2021-06-12T09:00:00+00:00', 'actual': '2021-06-12T09:21:00+00:00', 'estimated_runway': '2021-06-12T09:21:00+00:00', 'actual_runway': '2021-06-12T09:21:00+00:00'}, 'arrival': {'airport': 'Kingsford Smith', 'timezone': 'Australia/Sydney', 'iata': 'SYD', 'icao': 'YSSY', 'terminal': '1', 'gate': '25', 'baggage': '15', 'delay': 3, 'scheduled': '2021-06-12T10:20:00+00:00', 'estimated': '2021-06-12T10:20:00+00:00', 'actual': '2021-06-12T10:23:00+00:00', 'estimated_runway': '2021-06-12T10:23:00+00:00', 'actual_runway': '2021-06-12T10:23:00+00:00'}, 'airline': {'name': 'Air New Zealand', 'iata': 'NZ', 'icao': 'ANZ'}, 'flight': {'number': '103', 'iata': 'NZ103', 'icao': 'ANZ103', 'codeshared': None}, 'aircraft': {'registration': 'ZK-NZN', 'iata': 'B789', 'icao': 'B789', 'icao24': 'C8273F'}, 'live': None}, {'flight_date': '2021-06-11', 'flight_status': 'landed', 'departure': {'airport': 'Auckland International', 'timezone': 'Pacific/Auckland', 'iata': 'AKL', 'icao': 'NZAA', 'terminal': 'I', 'gate': '6', 'delay': 22, 'scheduled': '2021-06-11T09:00:00+00:00', 'estimated': '2021-06-11T09:00:00+00:00', 'actual': '2021-06-11T09:21:00+00:00', 'estimated_runway': '2021-06-11T09:21:00+00:00', 'actual_runway': '2021-06-11T09:21:00+00:00'}, 'arrival': {'airport': 'Kingsford Smith', 'timezone': 'Australia/Sydney', 'iata': 'SYD', 'icao': 'YSSY', 'terminal': '1', 'gate': '37', 'baggage': '15', 'delay': None, 'scheduled': '2021-06-11T10:20:00+00:00', 'estimated': '2021-06-11T10:20:00+00:00', 'actual': '2021-06-11T10:14:00+00:00', 'estimated_runway': '2021-06-11T10:14:00+00:00', 'actual_runway': '2021-06-11T10:14:00+00:00'}, 'airline': {'name': 'Air New Zealand', 'iata': 'NZ', 'icao': 'ANZ'}, 'flight': {'number': '103', 'iata': 'NZ103', 'icao': 'ANZ103', 'codeshared': None}, 'aircraft': {'registration': 'ZK-NZN', 'iata': 'B789', 'icao': 'B789', 'icao24': 'C8273F'}, 'live': None}, {'flight_date': '2021-06-11', 'flight_status': 'landed', 'departure': {'airport': 'Auckland International', 'timezone': 'Pacific/Auckland', 'iata': 'AKL', 'icao': 'NZAA', 'terminal': 'I', 'gate': '1', 'delay': 19, 'scheduled': '2021-06-11T16:00:00+00:00', 'estimated': '2021-06-11T16:00:00+00:00', 'actual': '2021-06-11T16:19:00+00:00', 'estimated_runway': '2021-06-11T16:19:00+00:00', 'actual_runway': '2021-06-11T16:19:00+00:00'}, 'arrival': {'airport': 'Kingsford Smith', 'timezone': 'Australia/Sydney', 'iata': 'SYD', 'icao': 'YSSY', 'terminal': '1', 'gate': '37', 'baggage': '15', 'delay': 2, 'scheduled': '2021-06-11T17:35:00+00:00', 'estimated': '2021-06-11T17:35:00+00:00', 'actual': '2021-06-11T17:37:00+00:00', 'estimated_runway': '2021-06-11T17:37:00+00:00', 'actual_runway': '2021-06-11T17:37:00+00:00'}, 'airline': {'name': 'Air New Zealand', 'iata': 'NZ', 'icao': 'ANZ'}, 'flight': {'number': '109', 'iata': 'NZ109', 'icao': 'ANZ109', 'codeshared': None}, 'aircraft': None, 'live': None}]}
-
-
+#response = [{'flight_date': '2021-06-12', 'flight_status': 'landed', 'departure': {'airport': 'Auckland International', 'timezone': 'Pacific/Auckland', 'iata': 'AKL', 'icao': 'NZAA', 'terminal': 'I', 'gate': '9', 'delay': 10, 'scheduled': '2021-06-12T08:55:00+00:00', 'estimated': '2021-06-12T08:55:00+00:00', 'actual': '2021-06-12T09:04:00+00:00', 'estimated_runway': '2021-06-12T09:04:00+00:00', 'actual_runway': '2021-06-12T09:04:00+00:00'}, 'arrival': {'airport': 'Tullamarine', 'timezone': 'Australia/Melbourne', 'iata': 'MEL', 'icao': 'YMML', 'terminal': '2', 'gate': '5', 'baggage': None, 'delay': None, 'scheduled': '2021-06-12T10:45:00+00:00', 'estimated': '2021-06-12T10:45:00+00:00', 'actual': '2021-06-12T10:33:00+00:00', 'estimated_runway': '2021-06-12T10:33:00+00:00', 'actual_runway': '2021-06-12T10:33:00+00:00'}, 'airline': {'name': 'Air New Zealand', 'iata': 'NZ', 'icao': 'ANZ'}, 'flight': {'number': '123', 'iata': 'NZ123', 'icao': 'ANZ123', 'codeshared': None}, 'aircraft': None, 'live': None}, {'flight_date': '2021-06-12', 'flight_status': 'cancelled', 'departure': {'airport': 'Auckland International', 'timezone': 'Pacific/Auckland', 'iata': 'AKL', 'icao': 'NZAA', 'terminal': 'I', 'gate': '1', 'delay': None, 'scheduled': '2021-06-12T18:30:00+00:00', 'estimated': '2021-06-12T18:30:00+00:00', 'actual': None, 'estimated_runway': None, 'actual_runway': None}, 'arrival': {'airport': 'Tullamarine', 'timezone': 'Australia/Melbourne', 'iata': 'MEL', 'icao': 'YMML', 'terminal': '2', 'gate': None, 'baggage': None, 'delay': None, 'scheduled': '2021-06-12T20:30:00+00:00', 'estimated': '2021-06-12T20:30:00+00:00', 'actual': None, 'estimated_runway': None, 'actual_runway': None}, 'airline': {'name': 'Air New Zealand', 'iata': 'NZ', 'icao': 'ANZ'}, 'flight': {'number': '129', 'iata': 'NZ129', 'icao': 'ANZ129', 'codeshared': None}, 'aircraft': None, 'live': None}, {'flight_date': '2021-06-11', 'flight_status': 'landed', 'departure': {'airport': 'Auckland International', 'timezone': 'Pacific/Auckland', 'iata': 'AKL', 'icao': 'NZAA', 'terminal': 'I', 'gate': '4C', 'delay': 34, 'scheduled': '2021-06-11T08:55:00+00:00', 'estimated': '2021-06-11T08:55:00+00:00', 'actual': '2021-06-11T09:28:00+00:00', 'estimated_runway': '2021-06-11T09:28:00+00:00', 'actual_runway': '2021-06-11T09:28:00+00:00'}, 'arrival': {'airport': 'Tullamarine', 'timezone': 'Australia/Melbourne', 'iata': 'MEL', 'icao': 'YMML', 'terminal': '2', 'gate': '7', 'baggage': None, 'delay': 6, 'scheduled': '2021-06-11T10:45:00+00:00', 'estimated': '2021-06-11T10:45:00+00:00', 'actual': '2021-06-11T10:44:00+00:00', 'estimated_runway': '2021-06-11T10:44:00+00:00', 'actual_runway': '2021-06-11T10:44:00+00:00'}, 'airline': {'name': 'Air New Zealand', 'iata': 'NZ', 'icao': 'ANZ'}, 'flight': {'number': '123', 'iata': 'NZ123', 'icao': 'ANZ123', 'codeshared': None}, 'aircraft': {'registration': 'ZK-NZQ', 'iata': 'B789', 'icao': 'B789', 'icao24': 'C82741'}, 'live': None}, {'flight_date': '2021-06-11', 'flight_status': 'landed', 'departure': {'airport': 'Auckland International', 'timezone': 'Pacific/Auckland', 'iata': 'AKL', 'icao': 'NZAA', 'terminal': 'I', 'gate': '3', 'delay': 15, 'scheduled': '2021-06-11T15:40:00+00:00', 'estimated': '2021-06-11T15:40:00+00:00', 'actual': '2021-06-11T15:54:00+00:00', 'estimated_runway': '2021-06-11T15:54:00+00:00', 'actual_runway': '2021-06-11T15:54:00+00:00'}, 'arrival': {'airport': 'Tullamarine', 'timezone': 'Australia/Melbourne', 'iata': 'MEL', 'icao': 'YMML', 'terminal': '2', 'gate': '3', 'baggage': None, 'delay': None, 'scheduled': '2021-06-11T17:30:00+00:00', 'estimated': '2021-06-11T17:30:00+00:00', 'actual': '2021-06-11T17:29:00+00:00', 'estimated_runway': '2021-06-11T17:29:00+00:00', 'actual_runway': '2021-06-11T17:29:00+00:00'}, 'airline': {'name': 'Air New Zealand', 'iata': 'NZ', 'icao': 'ANZ'}, 'flight': {'number': '125', 'iata': 'NZ125', 'icao': 'ANZ125', 'codeshared': None}, 'aircraft': {'registration': 'ZK-NZK', 'iata': 'B789', 'icao': 'B789', 'icao24': 'C8236E'}, 'live': None}, {'flight_date': '2021-06-11', 'flight_status': 'cancelled', 'departure': {'airport': 'Auckland International', 'timezone': 'Pacific/Auckland', 'iata': 'AKL', 'icao': 'NZAA', 'terminal': 'I', 'gate': '4', 'delay': None, 'scheduled': '2021-06-11T18:30:00+00:00', 'estimated': '2021-06-11T18:30:00+00:00', 'actual': None, 'estimated_runway': None, 'actual_runway': None}, 'arrival': {'airport': 'Tullamarine', 'timezone': 'Australia/Melbourne', 'iata': 'MEL', 'icao': 'YMML', 'terminal': '2', 'gate': '7', 'baggage': None, 'delay': None, 'scheduled': '2021-06-11T20:30:00+00:00', 'estimated': '2021-06-11T20:30:00+00:00', 'actual': None, 'estimated_runway': None, 'actual_runway': None}, 'airline': {'name': 'Air New Zealand', 'iata': 'NZ', 'icao': 'ANZ'}, 'flight': {'number': '129', 'iata': 'NZ129', 'icao': 'ANZ129', 'codeshared': None}, 'aircraft': None, 'live': None}]
 
 bot = commands.Bot(command_prefix='$')
 
 TOKEN = 'ODUzMDk4MTc1MDQzNjAwNDA0.YMQblg.e2dd7DP28hs-I8HH3oy7dZeWORA'
-
 
 @bot.event
 async def on_ready():
@@ -28,39 +25,47 @@ async def hello(ctx):
 
 @bot.command()
 async def flight(ctx, *args):
-    #query = {'access_key':'c4223304f131787d840025bdc3570a46'}
-    #url = "http://api.aviationstack.com/v1/"
-    #if len(args) == 3:
-    #    query['airline_name'] = args[0]
-    #    query['dep_iata'] = args[1]
-    #    query['arr_iata'] = args[2]
+    query = {'access_key':'c4223304f131787d840025bdc3570a46'}
+    url = "http://api.aviationstack.com/v1/"
+    if len(args) == 3:
+        query['airline_name'] = args[0]
+        query['dep_iata'] = args[1]
+        query['arr_iata'] = args[2]
 
+    response = requests.get(url+"flights", params=query)
+    botResponse = ['```' + 'There are ' + str(len(response.json()['data'])) + ' flights with your parameters today:' + '\n']
 
-    #response = requests.get(url+"flights", params=query)
-    print(response['data'][0])
-    botResponse = ['```' + 'There are ' + str(len(response['data'])) + ' flights with your parameters today.' + '\n']
+    for i in range(len(response.json()['data'])):
+        accessVar = response.json()['data'][i]
+        flightNo = accessVar['flight']['icao']
+        depAirport = accessVar['departure']['airport']
+        depTerminal = accessVar['departure']['terminal']
+        depGate = accessVar['departure']['gate']
+        depTime = accessVar['departure']['scheduled'][11:][:5]
+        arrAirport = accessVar['arrival']['airport']
+        arrTerminal = accessVar['arrival']['terminal']
+        arrGate = accessVar['arrival']['gate']
+        try:
+            aircraft = accessVar['aircraft']['icao']
+            rego = accessVar['aircraft']['registration']
+        except TypeError:
+            aircraft = 'Unknown'
+            rego = 'N/A'
+        status = accessVar['flight_status']
+        arrTime = accessVar['arrival']['scheduled'][11:][:5]
 
-
-    for i in range(len(response['data'])):
-        accessVar = response['data'][i]
-
-
-        newFlight = [accessVar['flight']['icao'] + ' ' + accessVar['departure']['airport'] + ' Terminal: ' + accessVar['departure']['terminal'] + ' Gate: '
-                    + accessVar['departure']['gate'] + ' ' + accessVar['departure']['scheduled'][11:][:5] + ' ' ]
-
-
+        newFlight = ['\n' + flightNo + ' ' + depAirport + ' Terminal: ' + str(depTerminal) + ' Gate: '
+                    + str(depGate) + ' Scheduled Departure: ' + depTime +
+                    ' ' + arrAirport + ' Terminal: ' + str(arrTerminal) + ' Gate: ' + str(arrGate)
+                    + ' Aircraft: ' + aircraft + ' ' + rego + '\n'+
+                    'Status: ' + status + ' ' + arrTime + '\n']
         botResponse = botResponse + newFlight
 
+    botResponse = botResponse + ['```']
 
+    botResponse = "".join(botResponse)
 
-
-
-
-
-
-
-
-    await ctx.send(botResponse[0])
+    await ctx.send(botResponse)
 
 
     #await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
